69f40fb6820fd2fe31f17063167db0bc329a9726
Andrew Mullins <andrew.mullins@puppetlabs.com>
2015-08-04 16:52:08 -0700

    Added command line option to start at a certain index and rate throttler

--- find_coordinates.rb
+++ find_coordinates.rb
@@ -2,26 +2,44 @@ require 'rubygems'
 require 'bundler/setup'
 require 'csv'
 require 'openssl'
+require 'optparse'
 
 require 'geocoder'
 
 # Array to fill with hashes of ID and long/lat
 longlat = []
 
+# Read starting index from command line
+index = 0
+OptionParser.new do |opts|
+  opts.banner = "Usage: find_coordinates.rb [options]"
+  opts.on("-iINDEX", Integer, "Index to start from") do |n|
+    index = n
+  end
+end.parse!
+
+stopLine = index + 2500
+
 # Read all the lines except the first
-CSV.foreach('data/addresses.csv', headers:true) do |row|
+CSV.foreach('data/addresses.csv', headers:true).with_index(1) do |row, rowno|
   # Concatenate date fields into an address
   full_address = [row[1], row[2], row[3], row[4], row[5]].join(' ')
   id = row[0]
+  # If we're in the relevant range of addresses...
+  if (rowno > index and rowno <= stopLine)
+    # Search the address in whatever geocoding service
+    d = Geocoder.search(full_address)
 
-  # Search the address in whatever geocoding service
-  d = Geocoder.search(full_address)
-  # If nil is returned we assume the address wasn't found
-  if !d[0].nil?
-    # Extract the longitude and latitude
-    ll = d[0].data["geometry"]["location"]
-    # Insert hash into array
-    longlat << {:id => id, :full_address => full_address, :lat => ll['lat'], :lng => ll['lng']}
+    # If nil is returned we assume the address wasn't found
+    if !d[0].nil?
+      # Extract the longitude and latitude
+      ll = d[0].data["geometry"]["location"]
+      # Insert hash into array
+      longlat << {:id => id, :full_address => full_address, :lat => ll['lat'], :lng => ll['lng']}
+    end
+
+    # Wait a 5th of a second so we don't go over the Google API query limit
+    sleep(0.2)
   end
 end
 
